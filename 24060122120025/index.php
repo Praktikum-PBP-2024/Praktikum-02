939226f63370880865a1881e26eae684a3400b77
dimassyyudha <dimasyudhasaputra96@gmail.com>
2024-09-07 11:32:56 +0700

    php-24060122120025

--- 24060122120025/index.php
+++ 24060122120025/index.php
@@ -1,3 +1,56 @@
+<!-- 
+NAMA   : DIMAS YUDHA SAPUTRA
+NIM    : 24060122120025
+LAB    : D2
+MATERI : SINTAKS DASAR PHP
+TANGGAL: 07/09/2024
+-->
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hitung Rata Array</title>
+</head>
+<body>
 <?php
+	$array_mhs = array(
+		'Abdul' => array(89, 90, 54),
+		'Budi' => array(78, 60, 64),
+		'Nina' => array(67, 56, 84),
+		'Budi' => array(87, 69, 50),
+		'Budi' => array(98, 65, 74)
+	);
 
-echo "PRAKTIKAN INI BELUM MENGERJAKAN TUGASNYA";
+	function hitung_rata($array){
+		$rata = array_sum($array) / count($array);
+		return $rata;
+	}
+
+	function print_mhs($array_mhs){
+		foreach ($array_mhs as $nama => $nilai){
+			$rata = hitung_rata($nilai);
+			echo "<tr>
+            <td>$nama</td>
+            <td>$nilai[0]</td>
+            <td>$nilai[1]</td>
+            <td>$nilai[2]</td>
+            <td>$rata</td>
+       	 </tr>";
+		}
+	}
+	echo '<table border="1">';
+	echo '<tr>
+	<td>Nama</td>
+	<td>Nilai 1</td>
+	<td>Nilai 2</td>
+	<td>Nilai 3</td>
+	<td>Rata2</td>
+	</tr>';
+
+	print_mhs($array_mhs);
+	
+	echo '<table>';
+	?>
+</body>
+</html>
