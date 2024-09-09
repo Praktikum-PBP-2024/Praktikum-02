34f862dcc18b381707077deb668370bca9fd1774
Faisal Rizki <160577005+FaisalRizkiii@users.noreply.github.com>
2024-09-09 19:28:48 +0700

    Update index.php

--- 24060122130068/index.php
+++ 24060122130068/index.php
@@ -1,3 +1,42 @@
 <?php
+/*Menghitung rata rata*/
+function hitung_rata($array) {
+    $total = array_sum($array);         // menjumlahkan nilai
+    $rata = $total / sizeof($array);    // menghitung rata-rata
+    return $rata;
+}
 
-echo "PRAKTIKAN INI BELUM MENGERJAKAN TUGASNYA";
+/*Menampilkan data mahasiswa*/
+function print_mhs($array_mhs) {
+    echo "<table border='1'>";
+    echo "<tr>
+            <th>Nama</th>
+            <th>Nilai 1</th>
+            <th>Nilai 2</th>
+            <th>Nilai 3</th>
+            <th>Rata2</th>
+          </tr>";
+
+    foreach ($array_mhs as $nama => $nilai) {
+        echo "<tr>
+                <td>$nama</td>
+                <td>{$nilai[0]}</td>
+                <td>{$nilai[1]}</td>
+                <td>{$nilai[2]}</td>";
+        $rata2 = hitung_rata($nilai);
+        echo "<td>$rata2</td>";
+        echo "</tr>";
+    }
+    echo "</table>";
+}
+
+$array_mhs = array(
+	'Abdul' => array(89,90,54),
+	'Budi' => array(78, 60, 64),
+	'Nina' => array(67, 56, 84),
+	'Budi' => array(87, 69, 50),
+	'Budi' => array(98, 65, 74)
+);
+
+print_mhs($array_mhs);
+?>
